135103ada3a3b68b9abd8cd8645f813ac88bb89b
Find-Serendipity <find.serendipity@gmail.com>
2024-05-14 14:43:00 -0400

    made changes to the users db functoins, and added additional testing

--- db/users.cjs
+++ db/users.cjs
@@ -2,6 +2,17 @@
 const prisma = require("../client.cjs");
 const bcrypt = require("bcrypt");
 
+const getAge = (date_of_birth) => {
+  const today = new Date();
+  const birthDate = new Date(date_of_birth);
+  const age = today.getFullYear() - birthDate.getFullYear();
+  const m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+};
+
 // Create/POST
 
 const createUser = async ({
@@ -12,21 +23,36 @@ const createUser = async ({
   date_of_birth,
 }) => {
   try {
-    const plainTextPassword = password;
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(plainTextPassword, saltRounds);
+    const age = getAge(date_of_birth);
 
-    const newUser = await prisma.user.create({
-      data: {
-        name,
-        username,
-        email,
-        password: hashedPassword,
-        date_of_birth,
-      },
-    });
+    if (username.length > 50) {
+    } else if (username.length > 25) {
+      throw Error(`Your username is too long!`);
+    } else if (email.length > 75) {
+      throw Error(`Your email is too long!`);
+    } else if (password.length > 250) {
+      throw Error(`Your password is too long!`);
+    } else if (age < 13) {
+      throw Error(
+        `Thanks for your interest in registering! Please ask your guardian to help you register an account.`
+      );
+    } else {
+      const plainTextPassword = password;
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(plainTextPassword, saltRounds);
 
-    return newUser;
+      const newUser = await prisma.user.create({
+        data: {
+          name,
+          username,
+          email,
+          password: hashedPassword,
+          date_of_birth: date_of_birth.toISOString(),
+        },
+      });
+
+      return newUser;
+    }
   } catch (err) {
     console.log("Error creating user", err);
     throw err;
