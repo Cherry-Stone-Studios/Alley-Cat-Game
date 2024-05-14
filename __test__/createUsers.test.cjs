05bfa488827a544b0ba3952d816a8a3a68b165f8
Find-Serendipity <find.serendipity@gmail.com>
2024-05-14 16:35:00 -0400

    scores added

--- __test__/createUsers.test.cjs
+++ __test__/createUsers.test.cjs
@@ -1,7 +1,7 @@
-const { createUser } = require("./users.cjs");
+const { createUser } = require("../db/users.cjs");
 const { prismaMock } = require("../singleton.cjs");
 
-test("Should create a new user", async () => {
+test("Should create a valid new user", async () => {
   const user1 = {
     userId: 1,
     name: "Anusha Delightful",
@@ -23,10 +23,20 @@ test("Should create a new user", async () => {
   });
 });
 
-//   await expect(async () => {
-//     await createUser(user2);
-//   }).toThrow("Your username is too long!");
-// });
+test("Creating a user with a username too long throws an error", async () => {
+  const user2 = {
+    userId: 2,
+    name: "Nakayla Amazing",
+    username: "nakaylisthemostamazingandbesteverwoooooothisissolonghorray",
+    email: "nakaylamazing@cherrystonestudios.com",
+    password: "whaaat1",
+    date_of_birth: "1996-12-17T00:00:00",
+  };
+
+  prismaMock.user.create.mockReturnValue(user2);
+
+  await expect(createUser(user2)).rejects.toThrow(`Your username is too long!`);
+});
 
 test("Creating a user with a username too long throws an error", async () => {
   const user2 = {
