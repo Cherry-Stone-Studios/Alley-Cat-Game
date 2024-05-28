4badab9c4408ec555c0a211e08db62e9ddaa2d92
Find-Serendipity <find.serendipity@gmail.com>
2024-05-28 15:44:01 -0400

    api routes appear to be working correctly on the front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 import Player from "./SpriteAnimation.jsx";
 
 import { Route, Routes } from "react-router-dom";
 import Game from "./game";
 import Register from "./Register.jsx";
+
+const USER_API = `https://cherry-stone-studios.onrender.com/api/users/`;
+const SCORE_API = `https://cherry-stone-studios.onrender.com/api/scores/`;
+const ADMIN_API = `https://cherry-stone-studios.onrender.com/api/admin/`;
+const FRIENDS_API = `https://cherry-stone-studios.onrender.com/api/users/friends/`;
 
 function App() {
   return (
