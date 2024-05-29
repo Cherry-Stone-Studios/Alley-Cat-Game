478d3b2d2359941383bf999c09bb20c8d56ac44c
Valentino Ramiro <Valentinoramiro777@gmail.com>
2024-05-29 07:06:21 -0700

    stash

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,35 +1,58 @@
 import BackButton from "./Components/BackButton";
 import "./CSS/Register.css";
 import { useState } from "react";
+require("dotenv").config();
 
-const Form = () => {
+const Register = () => {
   // States for registration
   const [name, setName] = useState("");
-  const [Username, setUsername] = useState("");
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [DOB, setDOB] = useState("");
 
   // States for checking the errors
   const [submitted, setSubmitted] = useState(false);
   const [error, setError] = useState(false);
-};
 
-const handleName = (e) => {
-  setName(e.target.value);
-  setSubmitted(false);
-};
+  const handleName = (event) => {
+    setName(event.target.value);
+    setSubmitted(false);
+  };
 
-const handleRegister = async (event) => {
-  event.preventDefault();
-  const response = await fetch("/Register", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ name, username, password, email, date_of_birth }),
-  });
-  const data = await response.json();
-};
+  const handleUsername = (event) => {
+    setUsername(event.target.value);
+    setSubmitted(false);
+  };
 
-const Register = () => {
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+    setSubmitted(false);
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+    setSubmitted(false);
+  };
+
+  const handleDOB = (event) => {
+    setDOB(event.target.value);
+    console.log(event.target.value);
+    setSubmitted(false);
+  };
+
+  const handleRegister = async (event) => {
+    event.preventDefault();
+    console.log("in register handle");
+    const response = await fetch(`${DATABASE_URL}/api/users/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, username, password, email, DOB }),
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <>
       <BackButton />
@@ -40,35 +63,58 @@ const Register = () => {
             Name:
             <input
               type="text"
+              id="name"
               placeholder="Name"
               onChange={handleName}
-              value={name}
               required
             />
           </label>
           <label>
             Username:
-            <input type="text" placeholder="Username" required />
+            <input
+              type="text"
+              id="username"
+              placeholder="Username"
+              onChange={handleUsername}
+              required
+            />
           </label>
           <label>
             Email Address:
-            <input type="email" placeholder="Email" required />
+            <input
+              id="email"
+              type="email"
+              placeholder="Email"
+              onChange={handleEmail}
+              required
+            />
           </label>
 
           <label>
             Password:
-            <input type="password" placeholder="Password" required />
+            <input
+              type="password"
+              placeholder="Password"
+              onChange={handlePassword}
+              id="password"
+              required
+            />
           </label>
           <label>
             Confirm Password:
-            <input type="password" placeholder="Confirm Password" required />
+            <input
+              type="password"
+              id="retypedPassword"
+              placeholder="Confirm Password"
+              required
+            />
           </label>
           <label>
             Date of Birth:
-            <input type="date" required />
+            <input type="date" id="DOB" onChange={handleDOB} required />
           </label>
+          <button type="submit">Create a Cat!</button>
         </form>
-        <button type="submit">Create a Cat!</button>
         {/* MAKE THIS BUTTON LINK TO /LOGIN PAGE */}
         <button>Have an Account?</button>
       </div>
