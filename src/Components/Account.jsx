e5991c5850c747265c4ebfcb265f30a81241e190
Valentino Ramiro <Valentinoramiro777@gmail.com>
2024-05-31 13:00:55 -0700

    init commit of account page

--- src/Components/Account.jsx
+++ src/Components/Account.jsx
@@ -1,7 +1,64 @@
-export function Account() {
+import "../CSS/home.css";
+import React from "react";
+import backgroundMusic from "../assets/music/menu.mp3";
+import { Nav } from "./Nav.jsx";
+import GameInfo from "./GameInfo.jsx";
+
+export function Account({ userToken, username }) {
+  const bgMusic = new Audio(backgroundMusic);
+
+  const playMusic = async () => {
+    const confirmation = confirm(
+      "Hurry home, Alley Cat!\n\n (Cancel to stop music.)"
+    );
+    if (confirmation === true) {
+      try {
+        bgMusic.loop = true;
+        bgMusic.volume = 0.05;
+        bgMusic.play();
+      } catch (err) {
+        console.error(err);
+      }
+    } else if (confirmation === false) {
+      try {
+        bgMusic.loop = false;
+        bgMusic.pause();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  };
+
+  const stopMusic = async () => {
+    try {
+      bgMusic.loop = false;
+      bgMusic.pause();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <>
-      <h1>ACCOUNT</h1>
+      <h1 className="alleyHeader">{`WELCOME HOME, ${username}`}</h1>
+      <p className="alleyHome">
+        Prowl the alleys and collect fish in this purr-fect adventure!
+      </p>
+      <div onClick={() => stopMusic()}>
+        <Nav userToken={userToken} />
+        <GameInfo />
+      </div>
+      <img src={"/src/curiouscat.gif"} onClick={() => playMusic()} />
+      <div>
+        <h2 className="alleyHome">Global Leaderboard</h2>
+        {/* PLACE LEADERBOARD COMPONENT */}
+        {/* <Leaderboard /> */}
+        {/* PLACE USER LEADERBOARD COMPONENT */}
+        <h2 className="alleyHome">{`${username}'s Leaderboard`}</h2>
+        {/* <UserLeaderboard/> */}
+      </div>
+      <br></br>
+      <br></br>
     </>
   );
 }
