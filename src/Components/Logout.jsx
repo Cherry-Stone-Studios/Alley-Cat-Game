3ea1824942358ac8849bcf4ef71230222da06239
Find-Serendipity <find.serendipity@gmail.com>
2024-06-05 11:43:36 -0400

    Logout bug was the same as the delete account bug

--- src/Components/Logout.jsx
+++ src/Components/Logout.jsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-export function Logout({ setUserToken }) {
+export function Logout({ setUserToken, setUSername }) {
   const navigate = useNavigate();
 
   useEffect(() => {
     setUserToken(null);
+    setUSername("");
     navigate("/login");
   }, [navigate, setUserToken]);
 }
